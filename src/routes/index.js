92c3d3794cc0e293cc7f23f00b42f5c6f85580d6
rhuaannn <rhuanmarques1223@gmail.com>
2024-07-14 20:53:48 -0300

    feat: add jwt token

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,12 @@ const { Router } = require("express");
 const usersRoutes = require("./users.routes");
 const notesRouter = require("./notes.routes");
 const tagsRouter = require("./tags.routes");
+const sessionRouter = require("./sessions.routes");
 const routes = Router();
 routes.use("/users", usersRoutes);
 routes.use("/notes", notesRouter);
 routes.use("/tags", tagsRouter);
+routes.use("/session", sessionRouter);
 
 
 module.exports = routes;
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,13 +1,14 @@
 const { Router } = require("express");
 
 const UsersController = require("../controllers/UsersController");
+const ensureAuthenticated = require('../middlewares/ensureAuthenticated')
 
 const usersRoutes = Router();
 
 const usersController = new UsersController();
 
 usersRoutes.post("/", usersController.create);
-usersRoutes.put("/:id", usersController.update);
+usersRoutes.put("/", ensureAuthenticated, usersController.update);
 usersRoutes.get("/", usersController.getUsers);
 usersRoutes.get("/:id", usersController.getUsers);
 usersRoutes.delete("/:id", usersController.deleteUsers);
