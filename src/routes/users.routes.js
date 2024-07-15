c9ee43961e1d4b282ec04bc25d369dff37a4712c
rhuaannn <rhuanmarques1223@gmail.com>
2024-07-15 20:26:26 -0300

    feat: add upload image

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,9 +1,13 @@
 const { Router } = require("express");
+const multer = require("multer");
+const uploadConfig = require("../configs/upload");
 
 const UsersController = require("../controllers/UsersController");
 const ensureAuthenticated = require('../middlewares/ensureAuthenticated')
 
 const usersRoutes = Router();
+const upload = multer(uploadConfig.MULTER)
+
 
 const usersController = new UsersController();
 
@@ -12,6 +16,11 @@ usersRoutes.put("/", ensureAuthenticated, usersController.update);
 usersRoutes.get("/", usersController.getUsers);
 usersRoutes.get("/:id", usersController.getUsers);
 usersRoutes.delete("/:id", usersController.deleteUsers);
+usersRoutes.patch("/avatar",ensureAuthenticated, upload.single("avatar"), (req, res) => {
+    console.log(req.file.filename)
+    res.json()
+});
+
 
 
 module.exports = usersRoutes;
